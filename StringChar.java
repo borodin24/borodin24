7a9d5b940d15185eb1b0f285d2b38bccab42b789
borodin24 <eu.geniu1990@gmail.com>
2024-10-30 14:33:20 +0200

    characters

--- StringChar.java
+++ StringChar.java
@@ -11,10 +11,17 @@ public class StringChar {
         // a program that prints out the individual charcters astring :
         for (int i =0; i < m.length();i++ )
         {
-            System.out.println(m.charAt(i));
-        }
-
+        System.out.println(m.charAt(i)); }
         //Characters
+
+        char tt = 'j';
+        char u = 66;
+        char x ='\u03A9';
+        char y = '\b' ;
+        System.out.println(y);
+        System.out.println(tt);
+        System.out.println(u);
+        System.out.println(x);
 
 
 
